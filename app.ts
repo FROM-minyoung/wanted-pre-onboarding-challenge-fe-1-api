a6aaab8d0732ead4c66aa0ed098c3648fbbe2d33
FROM-minyoung <918liter@gmail.com>
2023-01-05 01:14:15 +0900

    Fix: cors 수정

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.use("/todos", todoRouter);
 app.use("/users", userRouter);
